ac9920be11f76f8f05be76d0dee52c2001209d51
Jiyeon Choi <jiyunee13@gmail.com>
2024-09-01 10:47:42 +0900

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+
+  // optional server config
+  server: {
+    port: 3000,
+  },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
